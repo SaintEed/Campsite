8dfa45c5c6b3b8ff4b69418b53eae595327beb73
SaintEed <dsteed18@gmail.com>
2021-03-31 17:06:29 +0100

    Init

---
+++ campsite/src/test/java/com/deusnex/campsite/CampsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusnex.campsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
