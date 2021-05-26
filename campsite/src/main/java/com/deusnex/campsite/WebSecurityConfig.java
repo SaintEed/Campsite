dc076f528a77da6061560a3ba0389e91fec8584e
SaintEed <dsteed18@gmail.com>
2021-05-26 12:06:30 +0100

    DB Security added

--- campsite/src/main/java/com/deusnex/campsite/WebSecurityConfig.java
+++ campsite/src/main/java/com/deusnex/campsite/WebSecurityConfig.java
@@ -1,19 +1,28 @@
 package com.deusnex.campsite;
 
-import org.springframework.context.annotation.Bean;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	DataSource dataSource;
+	
+	//Enable jdbc authentication
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+    
+    
 	@Override
 	protected void configure(HttpSecurity http) throws Exception{
 		http
@@ -28,17 +37,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 				.logout()
 						.permitAll();
 		}
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-			UserDetails admin =
-					User.withDefaultPasswordEncoder()
-							.username("Admin")
-							.password("password")
-							.roles("USER")
-							.build();
-							
-			return new InMemoryUserDetailsManager(admin);
-	}
+	
+	
+	
 	
 }
